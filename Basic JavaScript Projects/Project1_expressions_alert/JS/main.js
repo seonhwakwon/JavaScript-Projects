0f544fed0531cf3b19f2c69554d3f8ed5c38d3ec
sunnakwon <148311845+sunnakwon@users.noreply.github.com>
2023-11-14 14:39:54 -0800

    first commit for Basic JavaScript

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,25 @@
+function My_First_Function() {
+  var str ="This is the button text"
+  document.getElementById("Button_Text").innerHTML = str;
+} //created button and click to message 
+
+window.alert("Hello, World!"); //alert for Hello world
+
+document.write('Hello, World!<br>'); //write Hello World.
+
+var A = "This is a string.<br>"; //declear variable
+
+document.write(A); //write variable A
+document.write("Lisa told Bart, \"Knock it off, Bart. Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.<br>"); //write
+
+document.write("\"Be who you are and say what you feel,"
++"because those who mind don\'t matter and those who matter don\' mind.\""+"-Dr, Seuss<br>")
+
+var B = "Concatenated" +"String<br>" //decalare variable B
+document.write(B); //write the B
+
+var Family = "The Arezzinis", Dad = "Jeremiah<br>", Mom=" Hermoine", Daughter = "Penny", Son="Zorro"; //declear variable  Family, Dad, Mom, Daughter, Son
+document.write(Dad); //write variable Dad
+
+document.write(3+3); // write the the result 3+3
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # JavaScript-Projects
+
+This is a Repository for JavaScript Projects.

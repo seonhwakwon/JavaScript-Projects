92599d43d19577d411370b1512ebf4bce52b48f8
sunnakwon <148311845+sunnakwon@users.noreply.github.com>
2023-12-14 14:44:39 -0800

    twenty commit for Readme

--- Basic_JavaScript_Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic_JavaScript_Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,21 +1,21 @@
 function Vote_Function() {
   var Age, Can_vote; // declare variable age and Can_vote
   Age = document.getElementById("Age").value;// search id="age" and then get a input value into Age
-  Can_vote = (Age < 18 ) ? "You are not old enough to vote " : "You can vote."; //  if age < 18, put a value into Can_vote "You are too young", but if age is not age < 18, put a value into Can_vote "You are old enough". 
+  Can_vote = (Age < 18 ) ? "You are not old enough to vote " : "You can vote."; //  if age < 18, put a value into Can_vote "You are too young", but if age is not age < 18, put a value into Can_vote "You are old enough".(using ternary operator) 
   document.getElementById("Vote").innerHTML = Can_vote;//search id=vote and return "Can_vote" + to vote.
 }
 function Vehicle(Make, Model, Year, Color) {
-    this.Vehicle_Make = Make;
+    this.Vehicle_Make = Make; //
     this.Vehicle_Model = Model;
     this.Vehicle_Year = Year;
     this.Vehicle_Color =Color;
-}
-var Jack = new Vehicle("Dodge", "Viper",2020, "Red");
-var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
-var Erik = new Vehicle("Ford", "PInto", 1971, "Mustard");
+}//create constructor function "Vehicle"
+var Jack = new Vehicle("Dodge", "Viper",2020, "Red"); //create new object of Vehicle and put a new obect in varaible "Jack"
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");//create new object of Vehicle and put a new obect in varaible "Emily"
+var Erik = new Vehicle("Ford", "PInto", 1971, "Mustard");//create new object of Vehicle and put a new obect in varaible "Erik"
 function myFunction() {
   document.getElementById("New_and_This").innerHTML = "Erik drives a " +Erik.Vehicle_Color + "-colored" + Erik.Vehicle_Model +"manufactured in" + Erik.Vehicle_Year;
-}
+} //search id "New_and_This" and return "Erik drives a " +Erik.Vehicle_Color + "-colored" + Erik.Vehicle_Model +"manufactured in" + Erik.Vehicle_Year;
 
 function count_Function() {
   document.getElementById("Nested_Function").innerHTML = Count();
@@ -25,4 +25,4 @@ function count_Function() {
     Plus_one();
     return Starting_point;
   }
-}
+}//search id "Nested_function" return Function "Count()"and create Function "Count()" :  create Function "Plus_one()" and  increase +1 variable "Staring_point"(using Nested Function).
--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # JavaScript-Projects
 
-This is a Repository for JavaScript Projects.
+  There are some small projects I made using Javascript.
+
+## Projects
+
+..* Pizza Menu (Pizza_Project folder)
+..* Tic-Tac-Toe Game
+..* Calculator
+
+### Pizza Menu
+
+  This project is a menu used to order pizza with a various selection of toppings. It then takes the input information and calculates a price for the order.
+
+### Tic-Tae-Toe Game
+
+  The classic game of tic-tae-toe. It randomly rolls dice to select which of the two players goes first and then users are off playing the classic game. Users can decide which token each player wants to use and the winner is celebrated with music and flashing lights.
+
+### Calculator
+
+  A basic 4 function calculator. Users can add, subtract, multiply, and devide on a sleek web version of a calculator.
